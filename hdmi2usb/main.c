cd7416af63039151fcb4906c3ab13bbf481752ca
Jędrzej Boczar <jboczar@antmicro.com>
2019-12-04 16:38:34 +0100

    wip: trying to fix problem with endpoints being non-empty all the time

--- hdmi2usb/main.c
+++ hdmi2usb/main.c
@@ -22,16 +22,16 @@ int main() {
   fx2_usb_config();
 
   // Wait until FPGA sends DNA over CDC IN endpoint or timeout (EP can't be in auto IN FIFO mode!)
-  try_read_fpga_dna(200);
+  // try_read_fpga_dna(200);
 
-  // Reconfigure CDC IN endpoint in auto IN mode
-  SYNCDELAY; INPKTEND = USB_CFG_EP_CDC_DEV2HOST|_SKIP;
-  SYNCDELAY; FIFORESET = _NAKALL;
-  SYNCDELAY; FIFORESET = USB_CFG_EP_CDC_DEV2HOST|_NAKALL;
-  SYNCDELAY; EP_CDC_DEV2HOST(FIFOCFG) = _AUTOIN|_ZEROLENIN;
-  SYNCDELAY; EP_CDC_DEV2HOST(AUTOINLENH) = MSB(512);
-  SYNCDELAY; EP_CDC_DEV2HOST(AUTOINLENL) = LSB(512);
-  SYNCDELAY; FIFORESET = 0;
+  // // Reconfigure CDC IN endpoint in auto IN mode
+  // SYNCDELAY; INPKTEND = USB_CFG_EP_CDC_DEV2HOST|_SKIP;
+  // SYNCDELAY; FIFORESET = _NAKALL;
+  // SYNCDELAY; FIFORESET = USB_CFG_EP_CDC_DEV2HOST|_NAKALL;
+  // SYNCDELAY; EP_CDC_DEV2HOST(FIFOCFG) = _AUTOIN|_ZEROLENIN;
+  // SYNCDELAY; EP_CDC_DEV2HOST(AUTOINLENH) = MSB(512);
+  // SYNCDELAY; EP_CDC_DEV2HOST(AUTOINLENL) = LSB(512);
+  // SYNCDELAY; FIFORESET = 0;
 
   // Re-enumerate, to make sure our descriptors are picked up correctly.
   usb_init(/*disconnect=*/true);
@@ -40,6 +40,20 @@ int main() {
 
   while (1) {
     // slave fifos configured in auto mode
+
+    delay_ms(100);
+
+      // cdc_printf("EP2468CS = 0x%02x\r\n", EP2468STAT);
+      cdc_printf("EP2CS = 0x%02x\r\n", EP2CS);
+    delay_ms(100);
+      cdc_printf(" EP4CS = 0x%02x\r\n", EP4CS);
+    delay_ms(100);
+      cdc_printf("  EP6CS = 0x%02x\r\n", EP6CS);
+    delay_ms(100);
+      cdc_printf("   EP8CS = 0x%02x\r\n", EP8CS);
+    delay_ms(100);
+
+
   }
 }
 
@@ -70,19 +84,13 @@ void handle_usb_get_interface(uint8_t interface) {
 
 /*** USB registers configuration **********************************************/
 
+
+
 void fx2_usb_config() {
-  // NAK all transfers.
-  SYNCDELAY;
-  FIFORESET = _NAKALL;
+  /*** Slave FIFO interface configuration *************************************/
 
   // Use newest chip features required for auto slave FIFO operation (TRM 9.3.1)
   REVCTL = _ENH_PKT|_DYN_OUT;
-
-  // first all as invalid
-  EP2CFG &= ~_VALID;
-  EP4CFG &= ~_VALID;
-  EP6CFG &= ~_VALID;
-  EP8CFG &= ~_VALID;
 
   // Return FIFO setings back to default just in case previous firmware messed with them
   // Configure flags:
@@ -97,44 +105,53 @@ void fx2_usb_config() {
   // internal clock|48MHz|output to pin|normla polarity|syncronious mode|no gstate|slave FIFO interface mode [1:0]
   SYNCDELAY; IFCONFIG = _IFCLKSRC|_3048MHZ|_IFCLKOE|0|0|0|_IFCFG1|_IFCFG1;
 
+  /*** Endpoints configuration ************************************************/
+
+  // first all as invalid
+  EP2CFG &= ~_VALID;
+  EP4CFG &= ~_VALID;
+  EP6CFG &= ~_VALID;
+  EP8CFG &= ~_VALID;
+
   // CDC interrupt endpoint
   EP1INCFG = _VALID|_TYPE1|_TYPE0; // INTERRUPT IN.
   EP1OUTCFG &= ~_VALID; // EP1OUT not used
 
   // CDC 512-byte double buffed BULK OUT.
   EP_CDC_HOST2DEV(CFG) = _VALID|_TYPE1|_BUF1;
-  EP_CDC_HOST2DEV(CS) = 0;
-  SYNCDELAY; EP_CDC_HOST2DEV(FIFOCFG) = _AUTOOUT;
-  SYNCDELAY; EP_CDC_HOST2DEV(AUTOINLENH) = MSB(512);
-  SYNCDELAY; EP_CDC_HOST2DEV(AUTOINLENL) = LSB(512);
-
   // CDC 512-byte double buffed BULK IN.
   EP_CDC_DEV2HOST(CFG) = _VALID|_DIR|_TYPE1|_BUF1;
-  EP_CDC_DEV2HOST(CS) = 0;
-  SYNCDELAY; EP_CDC_HOST2DEV(FIFOCFG) = 0;
-  // no automatic committing as we need to first check for DNA
-
   // UVC 512-byte double buffered ISOCHRONOUS IN
   EP_UVC(CFG) = _VALID|_DIR|_TYPE0|_BUF1;
-  // FIFO: auto commit IN packets, set length of 512
-  SYNCDELAY; EP_UVC(FIFOCFG) = _AUTOIN|_ZEROLENIN;
-  SYNCDELAY; EP_UVC(AUTOINLENH) = MSB(512);
-  SYNCDELAY; EP_UVC(AUTOINLENL) = LSB(512);
-
   // UAC 512-byte double buffered ISOCHRONOUS IN
   EP_UAC(CFG) = _VALID|_DIR|_TYPE0|_BUF1;
-  SYNCDELAY; EP_UAC(FIFOCFG) = _AUTOIN|_ZEROLENIN;
-  SYNCDELAY; EP_UAC(AUTOINLENH) = MSB(512);
-  SYNCDELAY; EP_UAC(AUTOINLENL) = LSB(512);
 
-  // reset (and skip) endpoints
-  SYNCDELAY; FIFORESET = USB_CFG_EP_CDC_HOST2DEV|_NAKALL;
-  SYNCDELAY; OUTPKTEND = USB_CFG_EP_CDC_HOST2DEV|_SKIP;
-  SYNCDELAY; OUTPKTEND = USB_CFG_EP_CDC_HOST2DEV|_SKIP;
-  SYNCDELAY; FIFORESET = USB_CFG_EP_CDC_DEV2HOST|_NAKALL;
-  SYNCDELAY; FIFORESET = USB_CFG_EP_UVC|_NAKALL;
-  SYNCDELAY; FIFORESET = USB_CFG_EP_UAC|_NAKALL;
+  /*** FIFO configuration *****************************************************/
 
+  // NAK all transfers.
+  SYNCDELAY; FIFORESET = _NAKALL;
+  // reset endpoint fifos
+  SYNCDELAY; FIFORESET = USB_CFG_EP_CDC_HOST2DEV;
+  SYNCDELAY; FIFORESET = USB_CFG_EP_CDC_DEV2HOST;
+  SYNCDELAY; FIFORESET = USB_CFG_EP_UVC;
+  SYNCDELAY; FIFORESET = USB_CFG_EP_UAC;
   // restore normal operation
   SYNCDELAY; FIFORESET = 0;
+
+  // handle case when we were already in auto mode
+  // core needs to see AUTOOUT 0 to 1 transition to arm endpoints
+  SYNCDELAY; EP_CDC_HOST2DEV(FIFOCFG) = 0;
+
+  // configure auto mode for endpoints
+  SYNCDELAY; EP_CDC_HOST2DEV(FIFOCFG) = _AUTOOUT;
+  // SYNCDELAY; EP_CDC_DEV2HOST(FIFOCFG) = _AUTOIN|_ZEROLENIN;
+  SYNCDELAY; EP_CDC_DEV2HOST(FIFOCFG) = 0;
+  SYNCDELAY; EP_UVC(FIFOCFG) = _AUTOIN|_ZEROLENIN;
+  SYNCDELAY; EP_UAC(FIFOCFG) = _AUTOIN|_ZEROLENIN;
+
+  // configure auto packet length of IN endpoint FIFOs
+  SYNCDELAY; EP_UVC(AUTOINLENH) = MSB(512);
+  SYNCDELAY; EP_UVC(AUTOINLENL) = LSB(512);
+  SYNCDELAY; EP_UAC(AUTOINLENH) = MSB(512);
+  SYNCDELAY; EP_UAC(AUTOINLENL) = LSB(512);
 }
